b4d4ba30425c9904478488d31d3066c18744f354
Akmaral <136558349+arynttai@users.noreply.github.com>
2024-03-06 22:39:06 +0600

    Update Struct.swift

--- Delegatdz/Struct.swift
+++ Delegatdz/Struct.swift
@@ -19,14 +19,12 @@ struct HarryPotterCharacter {
     let surname: String
     let house: String
     let wand: String
-    // You can add more parameters as needed
 
     init(name: String, surname: String, house: String, wand: String) {
         self.name = name
         self.surname = surname
         self.house = house
         self.wand = wand
-        // Initialize more parameters here if necessary
     }
 }
 
